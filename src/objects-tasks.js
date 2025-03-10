e9d5b1ead4b7229c5a4d1a503db7366e56a85b07
Olena <olena.lightness@gmail.com>
2025-03-10 18:03:44 +0200

    feat: solve several problems

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -17,8 +17,9 @@
  *    shallowCopy({a: 2, b: { a: [1, 2, 3]}}) => {a: 2, b: { a: [1, 2, 3]}}
  *    shallowCopy({}) => {}
  */
-function shallowCopy(/* obj */) {
-  throw new Error('Not implemented');
+
+function shallowCopy(obj) {
+  return { ...obj };
 }
 
 /**
@@ -35,6 +36,14 @@ function shallowCopy(/* obj */) {
 function mergeObjects(/* objects */) {
   throw new Error('Not implemented');
 }
+// function mergeObjects(objects) {
+//   return objects.reduce((acc, curr => {
+//     for (const [key, value] of Object.entries(curr)) {
+//       acc[key] = (acc[key] || 0) + value;
+//     }
+//     return acc;
+//   }), {})
+// }
 
 /**
  * Removes a properties from an object.
@@ -49,8 +58,13 @@ function mergeObjects(/* objects */) {
  *    removeProperties({name: 'John', age: 30, city: 'New York'}, ['age']) => {name: 'John', city: 'New York'}
  *
  */
-function removeProperties(/* obj, keys */) {
-  throw new Error('Not implemented');
+// function removeProperties(/* obj, keys */) {
+//   throw new Error('Not implemented');
+// }
+function removeProperties(obj, keys) {
+  const copy = { ...obj };
+  keys.forEach((key) => delete copy[key]);
+  return copy;
 }
 
 /**
@@ -65,8 +79,11 @@ function removeProperties(/* obj, keys */) {
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 2}) => true
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 3}) => false
  */
-function compareObjects(/* obj1, obj2 */) {
-  throw new Error('Not implemented');
+// function compareObjects(/* obj1, obj2 */) {
+//   throw new Error('Not implemented');
+// }
+function compareObjects(obj1, obj2) {
+  return JSON.stringify(obj1) === JSON.stringify(obj2);
 }
 
 /**
@@ -80,8 +97,11 @@ function compareObjects(/* obj1, obj2 */) {
  *    isEmptyObject({}) => true
  *    isEmptyObject({a: 1}) => false
  */
-function isEmptyObject(/* obj */) {
-  throw new Error('Not implemented');
+// function isEmptyObject(/* obj */) {
+//   throw new Error('Not implemented');
+// }
+function isEmptyObject(obj) {
+  return Object.keys(obj).length === 0;
 }
 
 /**
@@ -100,8 +120,11 @@ function isEmptyObject(/* obj */) {
  *    immutableObj.newProp = 'new';
  *    console.log(immutableObj) => {a: 1, b: 2}
  */
-function makeImmutable(/* obj */) {
-  throw new Error('Not implemented');
+// function makeImmutable(/* obj */) {
+//   throw new Error('Not implemented');
+// }
+function makeImmutable(obj) {
+  return Object.freeze(obj);
 }
 
 /**
